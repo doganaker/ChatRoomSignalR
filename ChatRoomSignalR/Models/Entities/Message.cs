17750c5ceb532e76a709d0798ac323f577ec2d16
Doğan Aker <m.doganaker@gmail.com>
2021-02-03 19:24:09 +0300

    Message class prepared

---
+++ ChatRoomSignalR/Models/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoomSignalR.Models.Entities
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public Nullable<int> CallerID { get; set; }
+        public Nullable<int> ClientID { get; set; }
+    }
+}
