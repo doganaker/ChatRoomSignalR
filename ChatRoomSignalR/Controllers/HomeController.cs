2250addff19a8e33c37cad16c66c158b887abb9c
Doğan Aker <m.doganaker@gmail.com>
2021-02-05 16:25:50 +0300

    Issue #3 solved.

--- ChatRoomSignalR/Controllers/HomeController.cs
+++ ChatRoomSignalR/Controllers/HomeController.cs
@@ -19,16 +19,30 @@ namespace ChatRoomSignalR.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.id = Convert.ToInt32(HttpContext.User.Claims.ToArray()[1].Value);
             ViewBag.Name = HttpContext.User.Claims.ToArray()[0].Value;
             List<AdminUser> users = _chatcontext.AdminUsers.ToList();
 
             return View(users);
         }
 
-        public IActionResult User(int id)
+        public JsonResult User(int id)
         {
             var user = _chatcontext.AdminUsers.Find(id);
             return Json(user);
         }
+
+        [Route("GetChat/{callerId}/{clientId}")]
+        public JsonResult GetChat(int callerId, int clientId)
+        {
+            List<Message> chat = _chatcontext.Messages.Where(q => (q.CallerID == callerId && q.ClientID == clientId) || (q.CallerID == clientId && q.ClientID == callerId)).ToList();
+
+            return Json(chat);
+        }
     }
 }
+
+
+
+
+//https://localhost:44320/Home/GetChat/?callerId=10&clientId=1
